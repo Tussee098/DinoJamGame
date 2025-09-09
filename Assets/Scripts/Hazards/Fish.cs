3185eeaddcda75ba291556661ecaa66bc028d0aa
Tussee098 <Tusse098@hotmail.com>
2025-09-09 15:15:41 +0200

    feat: Fish+FishSpawner

--- Assets/Scripts/Hazards/Fish.cs
+++ Assets/Scripts/Hazards/Fish.cs
@@ -1,26 +1,35 @@
+using System;
 using UnityEngine;
 
-public class Fish : MonoBehaviour
-{
-    enum DirectionEnum
+namespace Hazards {
+    public enum DirectionEnum
     {
         Left, Right
     }
-    Fish(DirectionEnum direction)
+    public class Fish : MonoBehaviour
     {
-        m_direction = direction;
-    }
+        
+        public Fish(DirectionEnum direction)
+        {
+            m_direction = direction;
+        }
 
-    public float speed;
-    private DirectionEnum m_direction;
+        public float speed;
+        public DirectionEnum m_direction;
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
+            
+            float fDirection = 1f;
+            if (m_direction == DirectionEnum.Left) fDirection *= -1f;
 
-        float fDirection = 1f;
-        if (m_direction == DirectionEnum.Left) fDirection *= -1f;
+            transform.Translate(fDirection * speed * Time.deltaTime, 0, 0);
+        }
 
-        transform.Translate(0, fDirection * speed * Time.deltaTime, 0);
+        public void SetDirection(DirectionEnum direction)
+        {
+            m_direction = direction;
+        }
     }
 }
--- Assets/Scripts/Hazards/FishSpawner.cs
+++ Assets/Scripts/Hazards/FishSpawner.cs
@@ -1,34 +1,41 @@
 using System;
 using UnityEngine;
 
-public class FishSpawner : MonoBehaviour
-{
-
-    public float Cooldown = 3f;
-    public float m_currentCooldown;
-
-    [SerializeField] private GameObject fishPrefab;
-    [SerializeField] private Transform spawnPoint;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+namespace Hazards { 
+    public class FishSpawner : MonoBehaviour
     {
-        m_currentCooldown = Cooldown;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        m_currentCooldown -= Time.deltaTime;
-        if(m_currentCooldown <= 0f)
+        public float Cooldown = 3f;
+        public float m_currentCooldown;
+
+        public DirectionEnum direction;
+
+        [SerializeField] private GameObject fishPrefab;
+        [SerializeField] private Transform spawnPoint;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            SpawnFish();
             m_currentCooldown = Cooldown;
         }
-    }
 
-    private void SpawnFish()
-    {
-        GameObject newFish = Instantiate(fishPrefab, spawnPoint);
+        // Update is called once per frame
+        void Update()
+        {
+            m_currentCooldown -= Time.deltaTime;
+            if(m_currentCooldown <= 0f)
+            {
+                SpawnFish();
+                m_currentCooldown = Cooldown;
+            }
+        }
+
+        private void SpawnFish()
+        {
+            GameObject newFish = Instantiate(fishPrefab, spawnPoint);
+            Fish fishScript = newFish.GetComponent<Fish>();
+
+            fishScript.SetDirection(direction);
+        }
     }
 }
